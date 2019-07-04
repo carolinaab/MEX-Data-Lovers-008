c31c7d6b7981d2d4246ebae6473d60f46c6bcd73
Gabriela Barcena <g.barcena.v@gmail.com>
2019-07-04 17:02:29 -0500

    Modificaciones en RM.

--- src/data.js
+++ src/data.js
@@ -1,8 +1,36 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
 
-window.example = example;
+// window.example = example;
+
+//---- Filtrar la data ----//
+
+const data = POKEMON.pokemon;
+
+let str = "";
+data.forEach(element => {
+  const pokeNumber = element.num;
+  const pokeName = element.name;
+  const pokeType = element.type;
+  const pokeImg = element.img;
+
+  str += `<div class="poke-card">
+  <p id="p-number">${pokeNumber}</p>
+  <h3 id="p-name">${pokeName}</h3>
+  <p id="p-type">${pokeType}</p>
+  <img
+    src="${pokeImg}"
+    alt="Imagen del pokémon"
+    id="p-img"
+  />
+</div>`;
+});
+
+//---- Menú hamburguesa ----//
+const showMenu = () => {
+  principalMenu.classList.toggle("site-nav-open");
+};
