727fb11e32faa8aa757c1c1ca97dfdb84e7dc536
Gabriela Barcena <g.barcena.v@gmail.com>
2019-07-05 14:14:59 -0500

    modificaciones

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@
 
 // window.example = example;
 
-//---- Filtrar la data ----//
+//---- Mostrar la data ----//
 
 const data = POKEMON.pokemon;
 
@@ -39,3 +39,35 @@ const showMenu = () => {
 // const closeMenu = () => {
 //   principalMenu.classList.toggle("site-nav-close");
 // };
+
+//---- Imágen Preload ----//
+
+const containerPreload = document.getElementById("container-charge");
+
+const preloadCharge = () => {
+  containerPreload.style.display = "none";
+};
+
+window.onload = preloadCharge;
+
+//---- Aparecer y desaparecer secciones ----//
+const searchSection = document.getElementById("search");
+const welcomeSection = document.getElementById("welcome");
+const startButton = document.getElementById("button-start");
+const collectionSection = document.getElementById("collection-section");
+const menuWelcome = document.getElementById("home-m");
+const menuSearch = document.getElementById("search-m");
+const menuCollection = document.getElementById("collection-m");
+
+const showSearch = () => {
+  welcomeSection.classList.add("search-section");
+  searchSection.classList.remove("search-section");
+};
+
+const showCollection = () => {
+  collectionSection.classList.toggle("show-collection");
+};
+
+startButton.addEventListener("click", showSearch);
+
+menuWelcome.addEventListener("click", showCollection);
