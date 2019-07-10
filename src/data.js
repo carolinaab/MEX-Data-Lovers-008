85f8e4883b77ea7cfd1498cf95408a37bfdf7c5d
carolinaab <tau_karo@hotmail.com>
2019-07-10 01:12:33 -0500

    se agrega funcion de ordenar, ver detalles de funcionamiento

--- src/data.js
+++ src/data.js
@@ -11,53 +11,13 @@
 //   console.log(POKEMON.pokemon[pokemones].name)
 
 
-//
-const data = POKEMON.pokemon;
 
 
-// LISTA POKEMON
-const showCard = document.getElementById("data");
-
-const manipularData = (data) => {
-    let str = ""; //se crea variable que recibira la data 
-    data.forEach(element => { //recorrer la data similar al for 
-        str += `<div class="poke-card"> 
-       <img src="${element.img}"
-  alt="Imagen del pokémon"
-    id="p-img"</img>
-  <p id="p-number">${element.num}</p>
-  <h3 id="p-name">${element.name}</h3>
-  <p id="p-type">${element.type}</p>
-  
-  
-</div>`
-    });
-    showCard.innerHTML = str
-}
-manipularData(data)
 
 //---- Menú hamburguesa ----//
 const showMenu = () => {
     principalMenu.classList.toggle("site-nav-open");
 };
-// ordenar pokemones
-// const sortData = (data, sortBy, sortOrden) => {
-//     let listaOrdenada;
-//     switch (sortOrden) {
-//         case 'ascendente':
-//             if (sortBy === 'avf_spawns') {
-//                 listaOrdenada = data.sort((eleA, eleB) => eleA.avg_spawns - eleB.avg_spawns);
-//             } else {
-//                 listaOrdenada = data.sort((eleA, eleB) => eleA.name > eleB.name ? 1 : -1);
-//             }
-//             break;
-//         case 'descendente':
-//             listaOrdenada = data.sort((eleA, eleB) => eleB.name > eleA.name ? 1 : -1);
-//             break;
-//     }
-//     return listaOrdenada
-// };
-
 // BUSQUEDA DE POKEMONES 
 const mySearch = document.getElementById("my-search");
 const botonBuscar = document.getElementById("buscar");
@@ -66,3 +26,45 @@ botonBuscar.addEventListener('click', () => {
 const result = data.filter(pokemon => pokemon.name.toLocaleLowerCase() == mySearch)
 manipularData(result)
 });
+ 
+
+// ordenando la data de la A-Z y de Z-A
+const orderData = (data) => {
+    data.sort((a, b) => {
+      let nameA = a.name.toUpperCase();
+      // console.log(nameA)
+      let nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+          return -1;
+      }
+      if (nameA > nameB) {
+          return 1;
+      }
+      return 0;
+  });
+  return data;
+};
+// console.log(orderData)
+const orderDataZA = (data) => {
+    data.sort((a, b) => {
+        let nameA = a.name.toUpperCase();
+        let nameB = b.name.toUpperCase();
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        return 0;
+    });
+    return data.reverse();
+};
+
+
+
+window.data = {
+  orderData,
+  orderDataZA,
+  showMenu,
+};
+
--- src/main.js
+++ src/main.js
@@ -1,41 +1,66 @@
 
-//---- Mostrar la data ----//
-
-
-//---Menú Hamburquesa---//
-
+// se declaran todas las constantes para asi poder reutilizarlas
+const data = window.POKEMON.pokemon;
+const dataOrder = data;
+// console.log(dataOrder);
+const botonOrdenar = document.getElementById("ordenar");
+const showCard = document.getElementById("data");
 const buttonMenu = document.getElementById("icon-menu");
 const principalMenu = document.getElementById("nav");
 const buttonMenuOpen = document.getElementById("icon-menu-close");
-const theme = document.getElementById("theme-title")
-buttonMenu.addEventListener("click", showMenu);
+
+
+
+//---- Mostrar la data ----//
+
+
+// LISTA POKEMON
+
+
+const manipularData = (data) => {
+    let str = ""; //se crea variable que recibira la data 
+    data.forEach(element => { //recorrer la data similar al for 
+        str += `<div class="poke-card"> 
+       <img src="${element.img}"
+  alt="Imagen del pokémon"
+    id="p-img"</img>
+  <p id="p-number">${element.num}</p>
+  <h3 id="p-name">${element.name}</h3>
+  <p id="p-type">${element.type}</p>
+  
+  
+</div>`
+    });
+    showCard.innerHTML = str
+}
+manipularData(data)
+
+
+
+//---Menú Hamburquesa---//
+buttonMenu.addEventListener("click", showMenu); // eventos del menu
 
 
 
 
 // ordenar pokemones 
-// const ordenar = document.getElementById("ordenar")
-// const orderPokemon = document.getElementById("lista-ordenada");
-// const listaPokemon = document.getElementById("list-pokemon")
-// ordenar.addEventListener('change', () => {
-//     const selectOrder = orderPokemon.value;
-//     let sortOrder;
-//     switch (selectOrder) {
-//         case 'avg_spawns':
-//             sortOrder = 'ascendente';
-//             break;
-//         case 'az':
-//             sortOrder = 'ascendente';
-//             break;
-//         case 'za':
-//             sortOrder = 'descendente';
-//             break;
-//     }
-// return sortOrder;
-// });
 
-// const pokedexOrdenado = pokemon.sortData(data, selectOrder, sortOrder);
-;
-// listaPokemon.innerHTML = sortOrder;
+
+const getOrderValue = event => {
+    const orderValue = event.target.value;
+    
+    switch (orderValue) {
+        case 'az':
+          orderValue = 'ascendente';
+            break;
+        case 'za':
+            orderValue = 'descendente';
+            break;
+    }
+return orderValue;
+
+};
+botonOrdenar.addEventListener('change', getOrderValue);
+    
 
 
